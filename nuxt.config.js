2718078b70a5c7918b5a6bf2adf15578b4c2e821
Leonard Wittrock <leo@ww-s.co>
2022-10-28 22:29:59 +0200

    Config changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,6 +59,8 @@ export default {
     websocketURL: "ws://localhost:3001"
   },
 
+  
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
